e2a4146df31524084ecfa975ea0832d78c35cf36
acevedoantonio76 <acevedoantonio76@gmail.com>
2025-10-05 13:40:01 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-self.addEventListener('install', e => {
+seconst CACHE_NAME = "CACHE_V2";
+lf.addEventListener('install', e => {
   e.waitUntil(caches.open('inv-v2').then(c => c.addAll([
     './','./index.html','./styles.css','./app.js',
     './data/investors.json','./manifest.webmanifest','./assets/icon-512.png'
